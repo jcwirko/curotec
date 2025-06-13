93ce927273febaf569af901b6fee954de254cda5
Juan Pablo Blariza <juanblariza@gmail.com>
2025-06-14 00:34:01 +0200

    feat: add exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -18,6 +20,32 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+    public function render($request, Throwable $exception)
+    {
+        if ($request->expectsJson()) {
+
+            if ($exception instanceof ValidationException) {
+               return $this->responseJson([
+                    'error' => 'Validation failed',
+                    'details' => $exception->errors(),
+                ], 422);
+            }
+
+            if ($exception instanceof ModelNotFoundException) {
+                $model = class_basename($exception->getModel());
+
+                return $this->responseJson("$model Model Not found");
+            }
+
+            return $this->responseJson([
+                'error' => 'Internal Server Error',
+                'message' => config('app.debug') ? $exception->getMessage() : 'Something went wrong.',
+            ], 500);
+        }
+
+        return parent::render($request, $exception);
+    }
+
     /**
      * Register the exception handling callbacks for the application.
      */
@@ -26,5 +54,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    private function responseJson($message, $statusCode = 404, $data = []): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'data' => $data,
+        ], $statusCode);
     }
 }
